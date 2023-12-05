7d261b8851390c30a3653cef0e23527dbf1bc678
tatsu <tatsukichisan@protonmail.com>
2023-12-06 02:34:11 +0900

    #1

---
+++ app/src/main/java/ninja/tatsukichi/teracom/HelloController.java
@@ -0,0 +1,14 @@
+package ninja.tatsukichi.teracom;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
